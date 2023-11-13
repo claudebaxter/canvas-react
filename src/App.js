8b929dba27c3148fe788a10c292afe1dfb05c5ea
claudebaxter <e.baxter@dark-coin.io>
2023-11-12 23:14:23 -0600

    Init player in center of canvas, added assumed assets and jsx/css markup and styles to be used later.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,91 @@
 import { Canvas } from './Canvas.js';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <Canvas width={window.innerWidth} height={window.innerHeight} />
+    <React.Fragment>
+      <Canvas width={window.innerWidth} height={window.innerHeight} />
+    </React.Fragment>
   );
 }
 
 export default App;
+
+/*
+this will go above Canvas component eventually
+<div style={{
+                position: 'absolute',
+                color: 'red',
+                padding: '8px',
+                fontSize: '14px',
+                userSelect: 'none'
+            }}>
+                <span>Score: <span id="scoreEl">0</span></span>
+            </div>
+
+            <div id="modal" style={{
+                display: 'none',
+                position: 'absolute',
+                backgroundColor: 'white',
+                top: '50%',
+                transform: 'translate(-50%, -50%)',
+                left: '50%',
+                padding: '16px',
+                maxWidth: '500px',
+                width: '100%',
+                textAlign: 'center',
+                borderRadius: '15px'
+            }}>
+                <label style={{ fontSize: '14px', color: 'grey' }}>Game Over</label>
+                <h1 id="modalScore" style={{ fontSize: '48px', color: 'red', marginBottom: '0', marginTop: '8px' }}>0</h1>
+                <p style={{ color: 'grey', margin: '0', fontSize: '13px' }}>POINTS</p>
+                <button id="button" style={{
+                    marginTop: '12px',
+                    backgroundColor: 'blue',
+                    border: 'none',
+                    borderRadius: '15px',
+                    color: 'white',
+                    padding: '8px 16px',
+                    cursor: 'pointer'
+                }}>
+                    RESTART
+                </button>
+                <div className="switch-container">
+                    <label className="switch">
+                        <input type="checkbox" defaultChecked />
+                        <span className="slider round"></span>
+                        <span className="switch-label">Music On/Off</span>
+                    </label>
+                </div>
+            </div>
+
+            <div id="startModal" style={{
+                position: 'absolute',
+                backgroundColor: 'white',
+                top: '50%',
+                transform: 'translate(-50%, -50%)',
+                left: '50%',
+                padding: '16px',
+                maxWidth: '500px',
+                width: '100%',
+                textAlign: 'center',
+                borderRadius: '15px'
+            }}>
+                <h1 style={{ fontSize: '24px', color: 'red', marginBottom: '0', marginTop: '8px' }}>ASAteroids</h1>
+                <button id="startButton" style={{
+                    marginTop: '12px',
+                    backgroundColor: 'blue',
+                    border: 'none',
+                    borderRadius: '15px',
+                    color: 'white',
+                    padding: '8px 16px',
+                    cursor: 'pointer'
+                }}>
+                    START
+                </button>
+            </div>
+
+            <img id="playerImage" src="./icon.svg" style={{ display: 'none' }} />
+            <audio id="backgroundMusic" src="./Lexica-Tiger-Tracks.mp3" loop />
+*/
--- src/Canvas.js
+++ src/Canvas.js
@@ -1,20 +1,28 @@
-import { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 export function Canvas(props) {
     const canvasRef = useRef(null);
+    const [player, setPlayer] = useState(null);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
 
         //initial drawing on the canvas
-        context.fillStyle = 'red';
+        context.fillStyle = 'black';
         context.fillRect(0, 0, props.width, props.height);
+
+        //instantiate the player in canvas center on component mount
+        const centerX = props.width / 2;
+        const centerY = props.height / 2;
+        const newPlayer = new Player(centerX, centerY, 30, 'blue');
+        setPlayer(newPlayer);
 
         //clickHandler logic to update canvas
         const clickHandler = () => {
-            context.fillStyle = 'blue';
-            context.fillRect(0, 0, props.width, props.height);
+           /* example of logic to turn background blue on click event 
+           context.fillStyle = 'blue';
+            context.fillRect(0, 0, props.width, props.height);*/
         }
 
         //event listener to call clickHandler function
@@ -24,7 +32,29 @@ export function Canvas(props) {
         return () => {
             canvas.removeEventListener('click', clickHandler);
         }
-    }, []);
+    }, [props.width, props.height]);
+
+    useEffect(() => {
+        //draw player when player state variable is not null
+        if (player) {
+            const canvas = canvasRef.current;
+            const context = canvas.getContext('2d');
+
+            context.beginPath();
+            context.arc(player.x, player.y, player.radius, 0, Math.PI * 2, false);
+            context.fillStyle = player.color;
+            context.fill();
+        }
+    }, [player]);
 
     return <canvas ref={canvasRef} width={props.width} height={props.height} />;
+}
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
 }
