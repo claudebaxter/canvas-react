b31693b06839c8421c59e162d2e9086cfd2442e8
claudebaxter <e.baxter@dark-coin.io>
2023-11-21 16:22:40 -0600

    Adjusting projectile angles, and projectile/enemy colors.

--- src/Canvas.js
+++ src/Canvas.js
@@ -67,15 +67,15 @@ const clickHandler = (event, canvas, projectiles) => {
         event.clientX - canvas.width / 2)
 
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 5,
+        y: Math.sin(angle) * 5
     }
 
     projectiles.push(new Projectile(
         canvas.width / 2, 
         canvas.height / 2, 
         5, 
-        'red', 
+        'white', 
         velocity,
         canvas.getContext('2d')
     ));
@@ -96,7 +96,7 @@ const Canvas = () => {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
-        const player = new Player(canvas.width / 2, canvas.height / 2, 30, 'blue', context);
+        const player = new Player(canvas.width / 2, canvas.height / 2, 10, 'white', context);
 
         function spawnEnemies() {
             setInterval(() => {
@@ -113,7 +113,7 @@ const Canvas = () => {
                     y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
                 };
 
-                const color = 'green';
+                const color = `hsl(${Math.random() * 360}, 50%, 50%)`;
                 const angle = Math.atan2(
                     canvas.height / 2 - y,
                     canvas.width / 2 - x
