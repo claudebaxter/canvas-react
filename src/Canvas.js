69871f309a0961e8fce941e545309db3c568a5b5
claudebaxter <e.baxter@dark-coin.io>
2023-11-22 16:35:30 -0600

    Created projectile explosion on enemy/projectile collisision.

--- src/Canvas.js
+++ src/Canvas.js
@@ -40,6 +40,36 @@ class Projectile {
     }
 };
 
+const friction = 0.94;
+class Particle {
+    constructor(x, y, radius, color, velocity, context) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity;
+        this.alpha = 1;
+        this.context = context;
+    }
+    draw() {
+        this.context.save();
+        this.globalAlpha = this.alpha;
+        this.context.beginPath();
+        this.context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        this.context.fillStyle = this.color;
+        this.context.fill();
+        this.context.restore();
+    }
+    update() {
+        this.draw();
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y; 
+        this.alpha -= 0.01;
+    }
+};
+
 class Enemy {
     constructor(x, y, radius, color, velocity, context) {
         this.x = x;
@@ -84,8 +114,6 @@ const clickHandler = (event, canvas, projectiles) => {
     console.log('click', projectiles);
 };
 
-console.log('gsap', gsap);
-
 const Canvas = () => {
     const canvasRef = useRef(null);
     const animationFrame = useRef(null);
@@ -95,6 +123,7 @@ const Canvas = () => {
         const context = canvas.getContext('2d');
         const projectiles = [];
         const enemies = [];
+        const particles = [];
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
@@ -137,6 +166,14 @@ const Canvas = () => {
             context.fillRect(0, 0, canvas.width, canvas.height);
             player.draw();
 
+            particles.forEach((particle, index) => {
+                if (particle.alpha <= 0) {
+                    particles.splice(index, 1);
+                } else {
+                    particle.update();
+                }
+            });
+
             projectiles.forEach((projectile, index) => {
                 projectile.update();
 
@@ -165,7 +202,20 @@ const Canvas = () => {
                     
                     //projectile enemy hit detection
                     if (dist - enemy.radius - projectile.radius < 1) {
-                        if (enemy.radius - 10> 10) {
+                        for (let i = 0; i < enemy.radius * 2; i++) {
+                            particles.push(
+                                new Particle(
+                                    projectile.x, 
+                                    projectile.y, 
+                                    Math.random() * 2, 
+                                    enemy.color, 
+                                    {
+                                        x: (Math.random() - 0.5) * (Math.random() * 8), 
+                                        y: (Math.random() - 0.5) * (Math.random() * 8)
+                                    }, 
+                                    canvas.getContext('2d')))
+                        }
+                        if (enemy.radius - 10 > 5) {
                             gsap.to(enemy, {
                                 radius: enemy.radius - 10
                             });
